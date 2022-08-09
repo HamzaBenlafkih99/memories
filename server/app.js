18a63a5b0f9646df1782da4edd10b36f21f7f621
hamza benlafkih <hamzabenlafkih90@gmail.com>
2022-08-09 10:50:02 +0000

    finishing the backend endpoints

--- server/app.js
+++ server/app.js
@@ -1,24 +1,23 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 
 // routes
 import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
-
-app.use(express.json());
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRouter);
 
 export default app;
